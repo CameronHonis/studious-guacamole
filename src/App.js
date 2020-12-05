fd0d82d12ff0802816d80564fc73f80e299d228f
CameronHonis <cameron-04@hotmail.com>
2020-12-05 18:15:46 -0500

    audio test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
 function App() {
+  const ref = React.useRef({text: ""});
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        
+        onClick={e => {
+          window.speechSynthesis.speak(new SpeechSynthesisUtterance(ref.current.text));
+        }}
+      >
+        asdf
+      </button>
+      <input
+        type="text"
+        name="text"
+        onChange={e => ref.current.text = e.target.value}
+      />
     </div>
   );
 }
